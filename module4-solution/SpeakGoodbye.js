da199a96242670e26c9473b815d14df1c1570067
anishmystery <anishmistry312@gmail.com>
2016-07-18 20:30:50 +0530

    Add files via upload

---
+++ module4-solution/SpeakGoodbye.js
@@ -0,0 +1,10 @@
+(function (window) {
+  var byeSpeaker = {};
+  var speakword = "Goodbye ";
+  byeSpeaker.speak = function (name) {
+  console.log(speakword + name);
+}
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,10 @@
+(function (window) {
+  var helloSpeaker = {};
+  var speakword = "Hello ";
+  helloSpeaker.speak = function (name) {
+  console.log(speakword + name);
+}
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
